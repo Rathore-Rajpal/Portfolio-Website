6683d0ad95977439951963b168a41b6f8180f057
Rathore-Rajpal <rathorerajpal18112003@gmail.com>
2026-01-08 22:34:43 +0530

    favicon updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,12 +50,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="./favicon-32x32.png"
-        />
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🐞</text></svg>" />
         <meta property="og:title" content="Rajpal Singh Rathore - AI Automation Engineer" />
         <meta
           property="og:description"
